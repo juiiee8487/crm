f57e6da021289053ebb7765a57e2478ed1856ec8
Patel Juhi <juhir22495@gmail.com>
2020-06-22 12:27:41 +0530

    Update crm.php

--- crm.php
+++ crm.php
@@ -1,7 +1,7 @@
 <?php
 /*
 Plugin Name: CRM System
-Plugin URI: http://wordpress.org/
+Plugin URI: https://github.com/juiiee8487/crm.git
 Description: This is simple CRM system that will collect customer data and build customer profiles inside of the client’s WordPress Dashboard. They need to collect data from potential customers via a simple lead gen form and then have a list of customers that is easy to browse and keep track of.They want to be able to place this form anywhere via shortcode.
 Author: Juhi Patel
 Version: 1.0
